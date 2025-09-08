cab36fceba8fe8c310756dd118e693669c4785ce
Your Name <you@example.com>
2025-09-08 18:03:36 +0500

    gfhgf

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const button=document.querySelectorAll("button");
+const display = document.getElementById("display");
 
+let output="";
+
+button.forEach(btn =>{
+btn.addEventListener("click",(e)=>{
+const value=e.target.innerText;
+if(value=="="){
+output=eval(output);
+display.value=output;
+}else{
+output+=value;
+display.value=output;
+}
+})
+})
